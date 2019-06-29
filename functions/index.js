5c3bee3a2ae2faf78fa87259679fbc0e8d63b00e
Thomas Pan <thomastpan@gmail.com>
2019-06-29 15:51:27 -0700

    ceci nest pas es6 fool....

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,14 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+// const firebaseConfig = require('../.config')
+var serviceAccount = require("../.config");
 
-admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://dopaminebomb.firebaseio.com"
+});
+
 exports.helloWorld = functions.https.onRequest((req, res) => {
  response.send("Hello World!");
 });
