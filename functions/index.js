a81056f6977c9e29013b22c4b7e274ad378bbda0
Thomas Pan <thomastpan@gmail.com>
2019-06-29 15:51:27 -0700

    ceci nest pas es6 fool....

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const firebaseConfig = require('../.config')
 
-admin.initializeApp();
+admin.initializeApp(firebaseConfig);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
